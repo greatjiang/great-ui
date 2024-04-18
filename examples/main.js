78609378c0a79167d79d9544da282dcff328f87e
greatjiang <greatjiangemail@gmail.com>
2024-04-18 23:51:41 +0800

    package修改

--- examples/main.js
+++ examples/main.js
@@ -2,13 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '../components/css/index.scss'
 
-import GreatUI from '../components/lib'
+import GreatBrick from '../components/lib'
 // import GButton from '../components/lib/button'
 
 // console.log(GComponents.default)
 const app = createApp(App)
 
-app.use(GreatUI)
+app.use(GreatBrick)
 // app.use(GButton)
 
 app.mount('#app')
