e345bc204233c85c83c0176c0c8b27320b0bcbbb
greatjiang <greatjiangemail@gmail.com>
2024-04-19 15:07:17 +0800

    vitepress 配置 review

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: '/greatbrick/'
+  base: "/greatbrick/",
   title: "greatbrick",
   description: "greatbrick components",
   themeConfig: {
